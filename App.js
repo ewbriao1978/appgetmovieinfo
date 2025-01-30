3ddcd3dc1e5aff6f08af3dfaee6aff3ea3bfa0ec
ewbriao1978 <eduardo.briao@gmail.com>
2025-01-30 09:17:19 -0300

    GETAPIMOVIE

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+
+import api from './src/services/api';
+import Filmes from './src/components/Filmes'
+
 
 export default function App() {
+
+  const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() =>{
+    async function loadMovies() {
+      const response = await api.get('r-api/?api=filmes')
+      setFilmes(response.data)
+      setLoading(false)
+    }  
+  
+    loadMovies()
+  
+  },[]) 
+  
+
+if(loading){
+  return(
+    <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
+      <ActivityIndicator color="#121212" size={45} />
+    </View>
+
+
+  )
+}else{
+  
   return (
+   
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+     <FlatList
+     data={filmes}
+     keyExtractor={ item => String(item.id)}
+     renderItem={ ({ item })=> <Filmes data={item}/> } 
+     />
+     
     </View>
   );
+}//else
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
+
 });
